befdf90d2c3406df6c075ca11f691bee3f5ba508
shachi.rai <shachi.rai@accenture.com>
2022-04-27 17:25:19 +0530

    initial commit

---
+++ src/components/categories-list/CategoriesList.jsx
@@ -0,0 +1,14 @@
+import { useEffect } from "react"
+
+function CategoriesList() {
+
+    useEffect();
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default CategoriesList
---
+++ src/components/pages/landing-page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import CategoriesList from '../categories-list/CategoriesList'
+
+export default function LandingPage() {
+    return (
+        <div>
+            <CategoriesList></CategoriesList>
+        </div>
+    )
+}
